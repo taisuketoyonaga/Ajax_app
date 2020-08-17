679bbdf6a66f4bd16ab9e9989aa284a7fe611e23
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-08-17 16:52:54 +0900

    既読機能サーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,5 +8,17 @@ class PostsController < ApplicationController
   redirect_to action: :index
  end
 
+ def checked
+  post = Post.find(params[:id])
+    if post.checked #このcheckedはカラム名
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[id])
+    render json: { post: item }
+ end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+# 渡す情報が一意の情報であればpathパラメーターの方が適しています。
+  get 'posts/:id', to: 'posts#checked'
 end
 
