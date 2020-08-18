585082aa60fab3a89416795394ef3b837f339bee
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-08-18 12:48:05 +0900

    メモ投稿機能のフロント実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
- def index
-  @posts = Post.all.order(id: "DESC")
- end
 
- def create
-  post = Post.create(content: params[:content] , checked: false)
-  render json:{ post: post }
- end
+  def index
+    @posts = Post.all.order(id: "DESC")
+  end
 
- def checked
+  def create
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
+  end
 
-  post = Post.find(params[:id])
+  def checked
+    post = Post.find(params[:id])
     if post.checked then
       post.update(checked: false)
     else
@@ -18,8 +18,7 @@ class PostsController < ApplicationController
     end
 
     item = Post.find(params[:id])
-    render json: { post: item }
- end
+    render json:{ post: item }
+  end
 
 end
-
